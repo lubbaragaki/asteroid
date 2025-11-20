393ea4d0a46db3662c241779a8a8b42963c6e9c6
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-11-20 22:02:09 +0100

    String and int scalar parsing: Done

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,30 +1,29 @@
 package parser
 
 // Defining the grammar used to parse the yaml config file
-
-type Filepath = String
-def isFilepath(str: String) = {
-  path: Filepath
-  if (str.startsWith("./")) {
-    val path = str.asInstanceOf[Filepath]
-  } else {
-    str
-  }
-}
-type Scalar = String | Int | Filepath
-type Mapping[A] = Map[Scalar, A]
+type Scalar = String | Int
+type Mapping[A] = Map[Scalar, Node[A]]
 type Node[A] = Scalar | Mapping[A] | List[A]
 
 // A yaml document is simply a list of nodes
-case class Yaml(nodesList: List[Node], filename: String) {
-  def parseStringScalar(str: String): Option[String | Filepath]
-  def parseIntScalar(str: String): Option[Int]
-  def parseScalar(str: String): Option[Scalar]
-  def parseMapping(str: String): Option[Mapping[A]]
-  def parseList(str: String): Option[List[A]]
-  def parseNode(str: String): Option[List[Node[A]]]
-  // Intent: return a list of 1 element which is then added to
-  // the object's list of nodes using the ++ operator, returning
-  // a new list since 'List's in Scala are immutable
-  // If parseNode returns 'None' then there is an error in the file
+case class Yaml(nodesList: List[Node[Any]], filename: String) {
+  
+  def parseIntScalar(str: String): Option[Int] = {
+    try
+      Some(Integer.parseInt(str.trim))
+    catch
+      case e: Exception => None
+  }
+
+  def parseScalar(str: String): Option[Scalar] = {
+    if (parseIntScalar(str) == None) {
+      Some(str)
+    } else {
+      parseIntScalar(str)
+    }
+  }
+
+  def parseMapping(str: String): Option[Mapping[A]] = {}
+  def parseList(str: String): Option[List[A]] = {}
+  def parseNode(str: String): Option[Node[A]] = {}
  }
