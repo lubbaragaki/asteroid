74ec179e024bfe814c272fdaaf22d0cfc8f884a1
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-11-20 16:07:57 +0100

    Yaml case class definition with function declarations for parser

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,6 +1,7 @@
 package parser
 
 // Defining the grammar used to parse the yaml config file
+
 type Filepath = String
 def isFilepath(str: String) = {
   path: Filepath
@@ -14,9 +15,16 @@ type Scalar = String | Int | Filepath
 type Mapping[A] = Map[Scalar, A]
 type Node[A] = Scalar | Mapping[A] | List[A]
 
-case class Yaml()
-
-
-object Parser {
-
-}
+// A yaml document is simply a list of nodes
+case class Yaml(nodesList: List[Node], filename: String) {
+  def parseStringScalar(str: String): Option[String | Filepath]
+  def parseIntScalar(str: String): Option[Int]
+  def parseScalar(str: String): Option[Scalar]
+  def parseMapping(str: String): Option[Mapping[A]]
+  def parseList(str: String): Option[List[A]]
+  def parseNode(str: String): Option[List[Node[A]]]
+  // Intent: return a list of 1 element which is then added to
+  // the object's list of nodes using the ++ operator, returning
+  // a new list since 'List's in Scala are immutable
+  // If parseNode returns 'None' then there is an error in the file
+ }
