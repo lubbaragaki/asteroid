32a6e20229734387828cc7cf946fe607bc82409c
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-11-26 21:10:40 +0100

    Migrate to external yaml parsing library

--- build.sbt
+++ build.sbt
@@ -2,6 +2,7 @@ scalaVersion := "3.3.7"
 version := "1.12.0"
 name := "asteroid"
 libraryDependencies ++= Seq(
-        "com.lihaoyi" %% "fansi" % "0.5.1"
+        "com.lihaoyi" %% "fansi" % "0.5.1",
+        "org.virtuslab" %% "scala-yaml" % "0.3.1"
 )
 addCommandAlias("ci","compile;test;run")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,14 @@
-import parser.parseUtils
-import parser.yamlParser
+import org.virtuslab.yaml.*
 import scala.io.Source
+
+case class Output(path: String, format: String) derives YamlCodec
+case class Comments(single: Seq[String], multiline: Seq[String]) derives YamlCodec
+case class Config(build: Seq[String], run: Seq[String], comments: Comments, wordlists: Seq[String], outputs: Map[String, Output]) derives YamlCodec
 
 object Main {
   def main(args: Array[String]): Unit = {
     val fileContents = Source.fromFile("/home/saku/Code/scala/asteroid/src/main/resources/examples/asteroid.belt").mkString
-    val config: Option[Map[String, Any]] = yamlParser.parseMapping(fileContents) 
-    config match {
-      case Some(result) => println(result)
-      case None => println("Error occured")
-    }
+    val config = fileContents.as[Config] 
+    println(config)
   }
 }
