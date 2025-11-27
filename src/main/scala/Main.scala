cdb88d63459f981f1da89f433ee73da85284c10b
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-11-27 11:28:11 +0100

    Error handling: handle 'Either' return value from yaml parser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,8 +7,12 @@ case class Config(build: Seq[String], run: Seq[String], comments: Comments, word
 
 object Main {
   def main(args: Array[String]): Unit = {
+    var err = false
     val fileContents = Source.fromFile("/home/saku/Code/scala/asteroid/src/main/resources/examples/asteroid.belt").mkString
-    val config = fileContents.as[Config] 
-    println(config)
+    val config = fileContents.as[Config] match {
+      case Right(validConfig) => validConfig
+      case Left(error) => err = true; error
+    }
+    if(err == true) println(s"Error while parsing your configuration file:\n$config") else println(config)
   }
 }
